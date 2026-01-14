94502475be5ea3fc014b242ffa2c0145b0f698cc
andy <andyestevez27@gmail.com>
2026-01-13 21:30:57 -0500

    Added colored text, status & service/product to open ports

--- nmap_scan.py
+++ nmap_scan.py
@@ -1,13 +1,13 @@
 import nmap
+from colorama import Fore, Style
 
 nm = nmap.PortScanner()
-
 
 print("Enter IP to scan:")
 IP = input()
 print("Scan ports 1 to X:")
 ports = input()
-options = f"-sS -sV -p1-{ports} -sC scan_results"
+options = f"-sV -sC -p1-{ports} scan_results"
 
 nm.scan(IP, arguments=options)
 
@@ -18,7 +18,9 @@ for host in nm.all_hosts():
         print("Protocol: %s" % protocol)
         ports = nm[host][protocol]
         print("-"*50)
-        print(f"Open ports on the {IP}")
+        print(f"Listed ports on {IP}")
         for port, state in ports.items():
-            print("Port: %s\tService: %s" % (port, state['name']))
-
+            if state['state'] == 'open':
+                print("Port: %s\tStatus: %-20s\tService: %-10s\tProduct: %s" % (port, Fore.GREEN + state['state'] + Style.RESET_ALL, state['name'], state['product']))
+            else:
+                print("Port: %s\tStatus: %-20s\tService: %-10s\tProduct: %s" % (port, Fore.YELLOW + state['state'] + Style.RESET_ALL, state['name'], state['product']))
